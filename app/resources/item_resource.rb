6ec7d2d471e685b24a025c1a02dd1aa57941aac3
anonymous anonymous <atc5nc@virginia.edu>
2022-02-22 23:35:56 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,18 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :price, :float
+  attribute :photo, :string
+  attribute :location, :string
+  attribute :description, :string
+  attribute :category_id, :integer
+  attribute :status, :boolean
+  attribute :owner_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
