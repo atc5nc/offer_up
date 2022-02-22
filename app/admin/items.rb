c837d6501849e148d8ff4e60a3e40aadb74c9943
anonymous anonymous <atc5nc@virginia.edu>
2022-02-22 23:35:13 +0000

    Whitelist columns in Item for ActiveAdmin

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Item do
 
+ permit_params :title, :price, :photo, :location, :description, :category_id, :status, :owner_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
